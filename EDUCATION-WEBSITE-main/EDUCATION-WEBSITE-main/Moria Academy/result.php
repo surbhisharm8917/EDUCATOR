3a6ba9e95ce31d9e0a0a21d14eb919f101e67a8c
SURBHI SHARMA <138256315+surbhisharm8917@users.noreply.github.com>
2025-02-04 12:33:38 +0530

    Add files via upload

---
+++ EDUCATION-WEBSITE-main/EDUCATION-WEBSITE-main/Moria Academy/result.php
@@ -0,0 +1,80 @@
+﻿<!DOCTYPE html>
+<!--code by webdevtrick (webdevtrick.com) -->
+<html>
+
+<head>
+	<meta charset=UTF-8" />
+	
+	<title>PHP Quiz</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<h1>Result | Webdevtrick.com</h1>
+		
+        <?php
+            
+            $answer1 = $_POST['question-1-answers'];
+            $answer2 = $_POST['question-2-answers'];
+            $answer3 = $_POST['question-3-answers'];
+            $answer4 = $_POST['question-4-answers'];
+            $answer5 = $_POST['question-5-answers'];
+        
+            $totalCorrect = 0;
+            
+            if ($answer1 == "C") { $totalCorrect++; }
+            if ($answer2 == "D") { $totalCorrect++; }
+            if ($answer3 == "A") { $totalCorrect++; }
+            if ($answer4 == "B") { $totalCorrect++; }
+            if ($answer5 == "D") { $totalCorrect++; }
+            
+            echo "<div id='results'>$totalCorrect / 5 correct</div>";
+            
+        ?>
+	
+	</div>
+
+<style>
+/*
+	Code By Webdevtrick (https://webdevtrick.com)
+*/
+
+
+body{ font: 14px Georgia, serif; }
+
+#page-wrap { width: 500px; margin: 0 auto;}
+
+h1  { margin: 25px 0; font: 18px Georgia, Serif; text-transform: uppercase; letter-spacing: 3px; }
+
+#quiz input {
+    vertical-align: middle;
+}
+
+#quiz ol {
+   margin: 0 0 10px 20px;
+}
+
+#quiz ol li {
+   margin: 0 0 20px 0;
+}
+
+#quiz ol li div {
+   padding: 4px 0;
+}
+
+#quiz h3 {
+   font-size: 17px; margin: 0 0 1px 0; color: #666;
+}
+
+#results {
+    font: 44px Georgia, Serif;
+}
+</style>
+
+</body>
+
+</html>
